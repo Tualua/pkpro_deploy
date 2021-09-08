66b01fb43b57772634cd55057e97549f59090220
Tualua <dsp@mvdnet.org>
2021-09-08 09:01:33 +0800

    Added product.img for correct Centos installation

--- create_iso.sh
+++ create_iso.sh
@@ -5,6 +5,7 @@ rm -f ../iso/images/pkpro.img
 find . | cpio -c -o | gzip -9cv > ../iso/images/pkpro.img
 cd $SRCPWD
 cp -f boot/EFI/grub.cfg iso/EFI/BOOT/
+cp -f centos/product.img iso/images/
 
 cd $SRCPWD/iso
 genisoimage -U -r -v -T -J -joliet-long -V "PKPROINST" -volset "PKPROINST" -A "PKPROINST" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot -o ../PKPROUEK.iso .
